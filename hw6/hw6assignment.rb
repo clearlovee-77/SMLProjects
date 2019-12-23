6d03381aab06903356922d41c45f8f63eb572083
372338552 <372338552@qq.com>
2019-12-24 12:11:14 +1300

    enhancement 1 finished

--- hw6/hw6assignment.rb
+++ hw6/hw6assignment.rb
@@ -5,18 +5,45 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-
+  # class array holding all the pieces and their rotations
+  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+                  rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+                  [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                  [[0, 0], [0, -1], [0, 1], [0, 2]]],
+                  rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+                  rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+                  rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+                  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+                  
   # your enhancements here
-
+  
 end
 
 class MyBoard < Board
   # your enhancements here
-
+  def rotate
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, 2)
+    end
+    draw
+  end
 end
 
 class MyTetris < Tetris
   # your enhancements here
 
+  # creates a canvas and the board that interacts with it
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def key_bindings
+    super()
+    @root.bind('u', proc {@board.rotate})
+  end
 end
 
